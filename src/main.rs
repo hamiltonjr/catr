632d07f39294e36185eea8f011a38881c33ae1e7
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2022-12-05 15:32:24 -0300

    Feature 1: arguments validation is ready.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use catr::run;
 
 fn main() {
-    if let Err(e) = run() {
+    if let Err(e) = catr::get_args().and_then(catr::run) {
         eprintln!("{}", e);
         std::process::exit(1);
     }
