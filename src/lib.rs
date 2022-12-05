dd7ca360537342adb05456f09e0b23a287d1c52d
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2022-12-05 18:15:46 -0300

    Feature 3: all comments included. (documentation).

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,32 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
+// Result type 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
+/*
+ * This is the configuration structure. It gives a clearer meaning to this 
+ * data.
+ */
 #[derive(Debug)]
 pub struct Config {
-    _files: Vec<String>,
-    _number_lines: bool,
-    _number_nonblank_lines: bool,
+    _files: Vec<String>,            // file name list
+    _number_lines: bool,            // if the flag -n is passed
+    _number_nonblank_lines: bool,   // if the flag -b is passed
 }
 
+/*
+ * This function get the arguments using the clap crate (one of simplest for 
+ * working with command-line arguments). A vector of file names is built and
+ * other arguments are returned too.
+ */
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("catr")
         .version("0.1.0")
         .author("Hamilton G. Jr. <hamiltonjr2010@gmail.com>")
         .about("Rust cat")
+
+        // argument dash for redirected stdin
         .arg(
             Arg::with_name("files")
                 .value_name("FILE")
@@ -24,6 +36,8 @@ pub fn get_args() -> MyResult<Config> {
                 .multiple(true)
                 .default_value("-"),
         )
+
+        // argument n for nembered lines 
         .arg(
             Arg::with_name("number")
                 .short("n")
@@ -32,6 +46,8 @@ pub fn get_args() -> MyResult<Config> {
                 .takes_value(false)
                 .conflicts_with("number_nonblank"),
         )
+
+        // argument n for non-blank nembered lines 
         .arg(
             Arg::with_name("number_nonblank")
                 .short("b")
@@ -41,6 +57,7 @@ pub fn get_args() -> MyResult<Config> {
         )
         .get_matches();
 
+    // returns configuration given for the arguments
     Ok(Config {
         _files: matches.values_of_lossy("files").unwrap(),
         _number_lines: matches.is_present("number"),
@@ -48,6 +65,11 @@ pub fn get_args() -> MyResult<Config> {
     })
 }
 
+/*
+ * This function opens a file and make error handling for files. A file can be 
+ * unreadable, corrupted or unexistent: such problems would crash the program
+ * without an error handling.
+ */
 fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     match filename {
         "-" => Ok(Box::new(BufReader::new(io::stdin()))),
@@ -55,11 +77,18 @@ fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
     }
 }
 
+/*
+ * This function implements all the program logic. Given the files and other 
+ * arguments, it reads each file and shows it properly.
+ */
 pub fn run(config: Config) -> MyResult<()> {
+    // run for the file name list
     for filename in config._files {
         match open(&filename) {
+            // error handling for files
             Err(err) => eprintln!("{}: {}", filename, err),
             Ok(file) => {
+                // line numbering given for arguments
                 let mut last_num = 0;
                 for (line_num, line) in file.lines().enumerate() {
                     let line = line?;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+/*
+ * This is the main function. The arguments are read for get_args() and passed
+ * to run().
+ */
 fn main() {
     if let Err(e) = catr::get_args().and_then(catr::run) {
         eprintln!("{}", e);
